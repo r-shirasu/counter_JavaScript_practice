f0f6952c2279523564a10ab1e44803970941f210
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-21 15:00:02 +0900

    途中経過です

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 let count = 0;
 
 // id属性値・class属性値などを意識せずにHTML要素を取得
-const getCount = document.querySelector("value");
-const buttons = document.querySelector(".btn");
+const getCount = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
-buttons.forEach(fuction (btn) ) {
-
-}
-
+// 全てのボタンのイベントを取り出す処理
+btns.forEach((btns) => {
+  btn.addEventListner("click", () => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    value.textContent = count;
+  });
+});
