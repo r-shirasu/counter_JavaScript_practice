bb81e023ae5b581bfb629e9d4a657daf53289fdc
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-21 13:17:22 +0900

    配列とforeachメソッドメモするため

---
+++ app.js
@@ -0,0 +1,11 @@
+// 数値の設定
+let count = 0;
+
+// id属性値・class属性値などを意識せずにHTML要素を取得
+const getCount = document.querySelector("value");
+const buttons = document.querySelector(".btn");
+
+buttons.forEach(fuction (btn) ) {
+
+}
+
