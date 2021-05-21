80fa2c94d0fe983724b15be6e2f6e428b22d0aa2
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-21 16:35:52 +0900

    foreachメソッド確認

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ let count = 0;
 
 // id属性値・class属性値などを意識せずにHTML要素を取得
 const getCount = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
+// const btns = document.querySelectorAll(".btn");
+const btns = ["cat", "dog", "fox"];
 
 // 全てのボタンのイベントを取り出す処理
-btns.forEach((btns) => {
-  btn.addEventListner("click", () => {
+btns.forEach((item, items, itemss) => {
+  //   console.log("item", "items", "itemss");
+  console.log("item", item);
+  console.log("items", items);
+  console.log("itemss", itemss);
+  return;
+  //   console.log(btn);
+  btns.addEventListner("click", () => {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
       count--;
@@ -19,3 +26,15 @@ btns.forEach((btns) => {
     value.textContent = count;
   });
 });
+
+// ①item
+// 要素＝DECREASE
+// ②items  (index) 0
+// ③itemss
+// 配列　btns
+
+// ①item   //
+// 要素＝INCREASE
+// ②items  (index) 1
+// ③itemss
+// 配列　btns
