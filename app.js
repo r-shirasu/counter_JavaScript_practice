a7d9c08946f2fa08ad3368464837a6c57c082004
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-25 16:24:34 +0900

    一時保存

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ let count = 0;
 
 const buttons = document.querySelectorAll(".btn");
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const buttonStyle = e.currentTarget.classList;
-    if (buttonStyle.contains("increase")) {
-      count++;
-    }
-    const getCount = document.getElementById("value");
-    getCount.textContent = count;
-  });
+// buttons.forEach((btn) => {
+//   console.log(btn);
+btn.addEventListener("click", (e) => {
+  console.log(e);
+  const buttonStyle = e.currentTarget.classList;
+  console.log(buttonStyle);
 });
+// });
+
+// let count = 0;
+
+// const buttons = document.querySelectorAll(".btn");
+
+// buttons.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const buttonStyle = e.currentTarget.classList;
+//     if (buttonStyle.contains("increase")) {
+//       count++;
+//       console.log(count);
+//     }
+//     const getCount = document.getElementById("value");
+//     getCount.textContent = count;
+//   });
+// });
